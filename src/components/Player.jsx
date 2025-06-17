9823c42c13ca4da40be5b1e77b6d58d5bb94eb59
Matthew Aitken <matt@itken.net>
2025-06-17 12:26:46 +0100

    Add TimerChallenge component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,17 +1,21 @@
 import { useState, useRef } from "react";
 
 export default function Player() {
+  // declare the playerName as a ref for the input so the input can be accessed
   const playerName = useRef();
   const [ enteredPlayerName, setEnteredPlayerName ] = useState();
 
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
+    // clearing the input after clicking, although this is not declarative
+    playerName.current.value = '';
   }
 
   return (
     <section id="player">
       <h2>Welcome {enteredPlayerName ?? 'unknown player'}</h2>
       <p>
+        {/* using the red prop to enable access to all the inputs methods etc */}
         <input ref={playerName} type="text"/>
         <button onClick={handleClick}>Set Name</button>
       </p>
