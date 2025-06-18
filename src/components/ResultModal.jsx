8ade437114bd87b13988dee0978b3e2a79b4a62a
Matthew Aitken <matt@itken.net>
2025-06-18 11:55:09 +0100

    Add results modal and change logic in TimerChallenge.jsx

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,47 @@
+import { useImperativeHandle, useRef } from "react";
+
+export default function ResultModal({ ref, remainingTime, targetTime, onReset }) {
+
+    // using a new dialog ref and imperative handle makes this component
+    // more independent as the open handle is now called which then calls
+    // the showModal() which is related to the dialog element.
+    const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+    // exposing the open() method which detaches specific element methods
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        }
+    });
+
+    return <dialog ref={dialog} className="result-modal" onClose={onReset}>
+        {userLost && <h2>You lost</h2>}
+        {!userLost && <h2>Your score: {score}</h2>}
+        <p>
+            The target time was <strong>{targetTime} seconds.</strong>
+            </p>
+        <p>
+            You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong>.
+            </p>
+        <form method='dialog' onSubmit={onReset}>
+            <button>Close</button>
+        </form>
+    </dialog>;
+}
+
+// in older versions of React you can forward refs so you need to..
+// note that the ref prop is now an additional parameter
+
+// import { forwardRef } from "react";
+
+// const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+//     // ...code
+// })
+
+// export default ResultModal;
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,37 +1,54 @@
 import { useState, useRef } from "react";
 
+import ResultModal from "./ResultModal";
+
 export default function TimerChallenge({ title, targetTime }) {
     const timer = useRef();
+    const dialog = useRef();
 
-    const [ timerStarted, setTimerStarted ] = useState(false);
-    const [ timerExpired, setTimerExpired ] = useState(false);
+    const [ timeRemaining, setTimeRemaining ] = useState(targetTime * 1000);
+
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+    if (timeRemaining <= 0) {
+        clearInterval(timer.current);
+        dialog.current.open();
+    }
+
+    // this is called from the onReset prop which is linked to the form submit
+    // so that the time is not reset before the modal is shown
+    function handleReset() {
+        setTimeRemaining(targetTime * 1000);
+    }
 
     // the targeTime prop is multiplied by 1000 to get the milliseconds
     function handleStart() {
-        setTimerStarted(true);
-        timer.current = setTimeout(() => {
-            setTimerExpired(true);
-        }, targetTime * 1000);
+        timer.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+        }, 10);
     }
 
     function handleStop() {
-        clearTimeout(timer.current);
+        dialog.current.open();
+        clearInterval(timer.current);
     }
 
-    return <section className="challenge">
-        <h2>{title}</h2>
-        {/* showing the You Lost! message conditionally on timerExpired state */}
-        {timerExpired && <p>You Lost!</p>}
-        <p className="challenge-time">
-            {targetTime} second{targetTime > 1 ? 's' : ''}
-        </p>
-        <p>
-            <button  onClick={timerStarted ? handleStop : handleStart}>
-                {timerStarted ? 'Stop' : 'Start'} Challenge
-            </button>
-        </p>
-        <p className={timerStarted ? 'active' : undefined}>
-            {timerStarted ? 'Time is running...' : 'Timer inactive'}
-        </p>
-    </section>
+    return <>
+        {/* ResultModal is like a normal modal, not visible until showModal() is called via ref */}
+        <ResultModal ref={dialog} remainingTime={timeRemaining} targetTime={targetTime} onReset={handleReset}/>
+        <section className="challenge">
+            <h2>{title}</h2>
+            <p className="challenge-time">
+                {targetTime} second{targetTime > 1 ? 's' : ''}
+            </p>
+            <p>
+                <button  onClick={timerIsActive ? handleStop : handleStart}>
+                    {timerIsActive ? 'Stop' : 'Start'} Challenge
+                </button>
+            </p>
+            <p className={timerIsActive ? 'active' : undefined}>
+                {timerIsActive ? 'Time is running...' : 'Timer inactive'}
+            </p>
+        </section>
+    </>
 }
