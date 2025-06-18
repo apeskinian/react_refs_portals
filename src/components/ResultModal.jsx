d1bb424992cdeb51b0ea47b11d18c1505832215b
Matthew Aitken <matt@itken.net>
2025-06-18 12:04:56 +0100

    Use portal to render the modal in the modal div

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useImperativeHandle, useRef } from "react";
+import { createPortal } from 'react-dom';
 
 export default function ResultModal({ ref, remainingTime, targetTime, onReset }) {
 
@@ -20,19 +21,23 @@ export default function ResultModal({ ref, remainingTime, targetTime, onReset })
         }
     });
 
-    return <dialog ref={dialog} className="result-modal" onClose={onReset}>
-        {userLost && <h2>You lost</h2>}
-        {!userLost && <h2>Your score: {score}</h2>}
-        <p>
-            The target time was <strong>{targetTime} seconds.</strong>
-            </p>
-        <p>
-            You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong>.
-            </p>
-        <form method='dialog' onSubmit={onReset}>
-            <button>Close</button>
-        </form>
-    </dialog>;
+    // createPortal will place the first parameter jsx code into the element in the second parameter!
+    return createPortal(
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
+            {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your score: {score}</h2>}
+            <p>
+                The target time was <strong>{targetTime} seconds.</strong>
+                </p>
+            <p>
+                You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong>.
+                </p>
+            <form method='dialog' onSubmit={onReset}>
+                <button>Close</button>
+            </form>
+        </dialog>,
+        document.getElementById('modal')
+    );
 }
 
 // in older versions of React you can forward refs so you need to..
